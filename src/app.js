b9ecfb21a3142c0e4ec41d90f40df3fb811b6fc3
Diego cervantes <diegocerbta@gmail.com>
2025-12-16 02:00:52 +0000

    first

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function DomainGeneretor () {let pronoun = ['the', 'our', 'hola'];
+let adj = ['great', 'big', 'miracle'];
+let noun = ['jogger', 'racoon', 'grandioso'];
+let terminacion = ['.com', '.net', 'algo']
+let Generador = []
+for ( let i = 0; i < pronoun.length; i++){
+  for (let k = 0; k < adj.length; k++){
+    for (let j = 0; j < noun.length; j++){
+      for (let h = 0; h < terminacion.length; h++){
+        Generador.push(pronoun[i] + adj[k] + noun[j] + terminacion[h])
+      }
+    }
+  }
+}
+return Generador
+}
+console.log(DomainGeneretor());
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
